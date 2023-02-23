a2c91e07914ea0fe9c6fdcfcac39714bbb44e35f
Niko <odasgardin@gmail.com>
2023-02-23 14:46:11 +0100

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,44 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+// Анимация Контента
+import AOS from 'aos';
+AOS.init();
+
+let menuElem = document.getElementById('sweeties');
+let titleElem = menuElem.querySelector('.caption');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};
+
+
+
+/*===================================search=============================================================== */
+const icon = document.querySelector('.icon')
+const search = document.querySelector('.search')
+const clear = document.querySelector('.clear')
+
+icon.onclick = function() {
+    search.classList.toggle('active')
+}
+
+clear.onclick = function() {
+    document.getElementById('mySearch').value = ''
+}
+
+
+/*===================================grid=============================================================== */
+
+$(document).ready(function(){
+  $(".row__content").slice(0, 3).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".row__content:hidden").slice(0, 3).slideDown();
+    if($(".row__content:hidden").length == 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+  
+})
